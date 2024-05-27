9a2125f1815284d8e92123e056671ce3826e4556
曹瀚文 <hanwen.cao@xtransfer.cn>
2024-05-27 19:33:57 +0800

    feat: init

---
+++ server/src/auth/auth.guard.ts
@@ -0,0 +1,55 @@
+import {
+  ExecutionContext,
+  UnauthorizedException,
+  CanActivate,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from 'src/public.decorator';
+
+type AuthorizedRequest = Request & { headers: { authorization: string } };
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      // 查看此条件
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(
+    request: AuthorizedRequest,
+  ): string | undefined {
+    const [type, token] = request.headers.authorization.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
