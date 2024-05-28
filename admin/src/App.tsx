0fb6661461a25dc6c96be4b1285734cc5c0c3b34
曹瀚文 <hanwen.cao@xtransfer.cn>
2024-05-28 15:30:07 +0800

    feat: init

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,25 +1,11 @@
 import React from "react"
-import "./App.css"
 import { ConfigProvider, Button } from "antd"
 
 function App() {
   return (
     <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.ssssxxx
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button type="primary">Button</Button>
-        </header>
+        <Button type="primary">Button</Button>
       </div>
     </ConfigProvider>
   )
---
+++ admin/src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import type { FormProps } from "antd"
+import { Button, Checkbox, Form, Input } from "antd"
+
+function Dashboard(props) {
+  return <div>Dashboard</div>
+}
+
+export default Dashboard
